1b995d16f75007176c2054611258747527977f94
pillar <ymblender@gmail.com>
2023-10-04 18:38:57 -0700

    feat: add animate-text

--- src/components/animate-text.jsx
+++ src/components/animate-text.jsx
@@ -1,14 +1,51 @@
+import { useEffect, useState } from "react";
+import { twMerge } from "tailwind-merge";
+
 export function AnimateText({ texts }) {
+  const [step, setStep] = useState(0);
+  const [fadeIn, setFadeIn] = useState(true);
+  const [textLength, setTextLength] = useState(0);
+  const { title, info } = texts[step];
+  useEffect(() => {
+    let fadeTimer;
+    const textLengthTimer = setInterval(() => {
+      setTextLength((v) => {
+        return v + 1;
+      });
+    }, 50);
+    const stepTimer = setInterval(() => {
+      setFadeIn(false);
+      fadeTimer = setTimeout(() => {
+        setFadeIn(true);
+        setTextLength(0);
+        setStep((v) => {
+          return (v + 1) % texts.length;
+        });
+      }, 500);
+    }, 4000);
+    return () => {
+      clearInterval(stepTimer);
+      clearTimeout(fadeTimer);
+      clearInterval(textLengthTimer);
+    };
+  }, [texts.length]);
   return (
-    <div>
-      {texts.map(({ title, info }) => {
-        return (
-          <>
-            <strong>{title}</strong>
-            <div>{info}</div>
-          </>
-        );
-      })}
+    <div
+      className={twMerge(
+        "transition-all duration-500",
+        fadeIn ? "" : "translate-y-10 opacity-0"
+      )}
+    >
+      <strong>{title}</strong>
+      <div className="h-32 mt-4">
+        {info.slice(0, textLength)}
+        <span
+          className={twMerge(
+            "inline-block w-8 h-8 bg-orange-700 rounded-full ml-4 translate-y-1 transition-opacity duration-200"
+          )}
+          style={{ animation: "fadeInOut 2s infinite" }}
+        ></span>
+      </div>
     </div>
   );
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -30,29 +30,31 @@ export default function Home() {
             texts={[
               {
                 title: "Help me pick",
-                info: "aaaaa aa aaaafd ksafj dklsfj",
+                info: "a gift for my dad who loves fishing",
               },
               {
-                title: "Help me pick2",
-                info: "aaaaa aa aaaafd ksafj dklsfj",
+                title: "Write a text",
+                info: "a Python script automating daily reports",
               },
               {
-                title: "Help me pick3",
-                info: "aaaaa aa aaaafd ksafj dklsfj",
+                title: "Imporve my post",
+                info: "to bring to a potluck",
               },
             ]}
           />
         </div>
       </div>
-      <div className="flex-1 flex flex-col items-center justify-center  min-h-screen relative">
+      <div className="flex-1 flex flex-col items-center justify-center min-h-screen relative pb-32">
         <div className="sm:hidden flex flex-row items-center p-8 absolute left-0 top-0">
           <h1 className="font-bold text-2xl">ChatGPT</h1>
           <span className="inline-block w-6 h-6 bg-black ml-2 rounded-full"></span>
         </div>
         <div className="flex flex-col justify-center items-center gap-4 w-full p-10">
-          <h2 className="text-3xl font-bold mb-4">Get started</h2>
-          <AButton href="/account/login">Login</AButton>
-          <AButton href="/account/sign-up">Sign up</AButton>
+          <h2 className="text-3xl sm:text-5xl font-bold mb-4">Get started</h2>
+          <div className="w-full flex flex-col lg:flex-row gap-3">
+            <AButton href="/account/login">Login</AButton>
+            <AButton href="/account/sign-up">Sign up</AButton>
+          </div>
         </div>
         <div className="flex flex-row gap-2 items-center absolute bottom-6">
           <Icon width="30" icon="ri:openai-fill" />
